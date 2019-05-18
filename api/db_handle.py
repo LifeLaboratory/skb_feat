7b1c69b63ccb487d5fbb7274dd1faf7e1fad2029
madwayz <djurball@list.ru>
2019-05-19 04:26:15 +0700

    *ready back

---
+++ api/db_handle.py
@@ -0,0 +1,51 @@
+import sqlite3
+import hashlib
+
+general_db_file_location = "database_files/general.db"
+
+
+def db_cmp_passwd(login, passwd):
+    _conn = sqlite3.connect(general_db_file_location)
+    _c = _conn.cursor()
+
+    hash = _c.execute("select passwd from users where login = " + login + ";").fetchone()
+    result = hash[0][0] == hashlib.sha256(passwd.encode()).hexdigest()
+    _conn.close()
+
+    return result
+
+
+def db_check_user(login, passwd):
+    _conn = sqlite3.connect(general_db_file_location)
+    _c = _conn.cursor()
+
+    hash = _c.execute("select count(*) from users where login = " + login + ";").fetchall()
+    result = hash[0][0] == hashlib.sha256(passwd.encode()).hexdigest()
+    _conn.close()
+
+    return result
+
+
+def db_add_user(login, passwd, firstname, secondname, bday, hobby, sex, social_link):
+    _conn = sqlite3.connect(general_db_file_location)
+    _c = _conn.cursor()
+
+    _c.execute("insert into users values(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+               (None, login, hashlib.sha256(passwd.encode()).hexdigest(), firstname, secondname, bday, hobby, sex, social_link))
+
+    _conn.commit()
+    _conn.close()
+
+
+def db_delete_user(login):
+    _conn = sqlite3.connect(general_db_file_location)
+    _c = _conn.cursor()
+
+    _c.execute("delete from users where login =  '" + login + "'; ")
+
+    _conn.commit()
+    _conn.close()
+
+#print(verify("roma_lox", "degenerat"))
+
+#print(hashlib.sha256("degenerat".encode()).hexdigest())
--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 from app import app
 
+from flask import render_template, request, redirect, session, flash, url_for
+from forms import RegForm, LoginForm
+from db_handle import db_cmp_passwd, db_add_user
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def index():
+    form = LoginForm(request.form)
+    login = request.form['username']
+    password = request.form['password']
+    if db_cmp_passwd(login, password):
+        session['log_in'] = True
+    else:
+        flash('Неправильный пароль')
+    return render_template('register2.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    if request.method == "POST":
+        login = request.form['username']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        secondname = request.form['secondname']
+        bday = request.form['bday']
+        sex = request.form['sex']
+        hobbys = request.form['hobbys']
+        social_link = request.form['social_link']
+        flash('Test Flash')
+
+        if db_cmp_passwd(login, password):
+            db_add_user(login, password, firstname, secondname, bday, sex, hobbys, social_link)
+            redirect('/events')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session['log_in'] = False
+    redirect('/')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80', debug=False, threaded=True)
+    app.run(host='0.0.0.0', debug=False, threaded=True)
+
+
